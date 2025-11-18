ff01c551badc73c06c67e4c0782e220a4c782eb8
Subnau940 <grechmatteo68@gmail.com>
2025-11-18 13:38:45 +0100

    Update Projetspicturelexique2.tsx

--- src/pages/Projets.tsx
+++ src/pages/Projets.tsx
@@ -31,7 +31,7 @@ const Projets = () => {
         "Organisation du développement professionnel"
       ],
       github: "#",
-      demo: ["/1000016898.png"]
+      demo: ["/1000016898.png"],
     },
     {
       title: "Veille informatique en cybersécurité",
