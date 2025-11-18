bc299e424958c17a8f951e463c902d2264bc9f65
Subnau940 <grechmatteo68@gmail.com>
2025-11-18 11:55:31 +0100

    Update Projets.tsx

--- src/pages/Projets.tsx
+++ src/pages/Projets.tsx
@@ -14,38 +14,7 @@ const Projets = () => {
         "Travail en mode projet"
       ],
       github: "#",
-import { useState } from "react";
-
-export default function DemoButton() {
-  const images = [
-    "https://raw.githubusercontent.com/Subnau940/Portfolio-Matteo-GRECH/main/src/pages/1000016892.png",
-    "https://raw.githubusercontent.com/Subnau940/Portfolio-Matteo-GRECH/main/src/pages/1000016894.png"
-  ];
-
-  const [current, setCurrent] = useState(0);
-
-  const handleClick = () => {
-    setCurrent((prev) => (prev + 1) % images.length);
-  };
-
-  return (
-    <div>
-      <button 
-        onClick={handleClick} 
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-      >
-        Demo
-      </button>
-      <div style={{ marginTop: "10px" }}>
-        <img 
-          src={images[current]} 
-          alt="Preuve projet" 
-          style={{ width: "300px", borderRadius: "8px" }} 
-        />
-      </div>
-    </div>
-  );
-}
+      demo: "#"
     },
     {
       title: "Co-révision du lexique interne de l'entreprise",
@@ -59,7 +28,7 @@ export default function DemoButton() {
         "Organisation du développement professionnel"
       ],
       github: "#",
-      demo: "#"
+      demo: "#", 
     },
     {
       title: "Veille informatique en cybersécurité",
